73ac15e0f36e8045b6bd03ed0b211c6c6a23627a
jpsdr <jpsdr.psx@free.fr>
2022-11-07 18:59:51 +0100

    Add hdr mode to sbrc.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- source/x265cli.h
+++ source/x265cli.h
@@ -187,6 +187,7 @@ static const struct option long_options[] =
     { "sbrc",                 no_argument, NULL, 0 },
     { "sbrc-hyst",            no_argument, NULL, 0 },
     { "sbrc-aq5",             no_argument, NULL, 0 },
+    { "sbrc-hdr",             no_argument, NULL, 0 },
     { "no-sbrc",              no_argument, NULL, 0 },
     { "rc-grain",             no_argument, NULL, 0 },
     { "no-rc-grain",          no_argument, NULL, 0 },
