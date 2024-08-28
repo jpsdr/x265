5530d53fa800a9efe29460aeafcdfb36f27c70da
jpsdr <jpsdr.psx@free.fr>
2024-08-28 19:22:24 +0200

    Rewrite a small part, maybe a fix...?
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- source/abrEncApp.cpp
+++ source/abrEncApp.cpp
@@ -676,10 +676,16 @@ ret:
 
                     if (m_cliopt.framesToBeEncoded && inFrameCount >= m_cliopt.framesToBeEncoded)
                         pic_in[view] = NULL;
-                    else if (readPicture(pic_in[view], view) && view == m_param->numViews - 1)
-                        inFrameCount++;
-                    else if (!pic_in[view])
-                        pic_in[view] = NULL;
+                    else
+                    {					
+						if (readPicture(pic_in[view], view))
+                        {
+							if (view == m_param->numViews - 1)
+                                inFrameCount++;
+                        }
+                        else
+                          pic_in[view] = NULL;
+                    }
                     if (pic_in[view])
                     {
                         if (pic_in[view]->bitDepth > m_param->internalBitDepth && m_cliopt.bDither)
